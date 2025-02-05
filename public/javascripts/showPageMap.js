5b1bd9d74f55e8d3db01d8c9459f58245ebb7109
sahilahmed21 <159174363+sahilahmed21@users.noreply.github.com>
2025-02-05 23:07:50 +0530

    Initial commit

---
+++ public/javascripts/showPageMap.js
@@ -0,0 +1,19 @@
+maptilersdk.config.apiKey = maptilerApiKey;
+
+const map = new maptilersdk.Map({
+    container: 'map',
+    style: maptilersdk.MapStyle.BRIGHT,
+    center: campground.geometry.coordinates, // starting position [lng, lat]
+    zoom: 10 // starting zoom
+});
+map.addControl(new mapboxgl.NavigationControl());
+
+new maptilersdk.Marker()
+    .setLngLat(campground.geometry.coordinates)
+    .setPopup(
+        new mapboxgl.Popup({ offset: 25 })
+            .setHTML(
+                `<h3>${campground.title}</h3><p>${campground.location}</p>`
+            )
+    )
+    .addTo(map)
